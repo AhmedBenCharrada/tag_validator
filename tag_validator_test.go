8e8b400ab7266044c4f8a254bf708e4936723ad1
ahmad <ahmed.b.charrada@gmail.com>
2022-06-27 16:30:47 +0100

    Modify tag validator

--- tag_validator_test.go
+++ tag_validator_test.go
@@ -8,12 +8,14 @@ import (
 
 func TestValidateStruct(t *testing.T) {
 	type User struct {
-		Id   string `validate:"uuid"`
-		Name string `validate:"string,min=2,max=10"`
-		Age  int    `validate:"number,min=18,max=20"`
+		Id     string `validate:"uuid"`
+		Name   string `validate:"string,min=2,max=10"`
+		Age    int    `validate:"number,min=18,max=20"`
+		Height int    `validate:"ignore,min=165,max=200"`
+		Weight int    `validate:"-"`
 	}
 
-	user := User{Id: "ba6516aa-3cb8-4592-b3cf-ba3ad9e176ae", Name: "name", Age: 19}
+	user := User{Id: "ba6516aa-3cb8-4592-b3cf-ba3ad9e176ae", Name: "name", Age: 19, Height: 180, Weight: 75}
 	errs := ValidateStruct("", user)
 
 	assert.Empty(t, errs)
