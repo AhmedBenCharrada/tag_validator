8cc5157ea2eb0ca38b4e56b8b6f571a9d4f98e8b
ahmad <ahmed.b.charrada@gmail.com>
2022-06-27 15:43:16 +0100

    Modify tests.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module tag_validator
 
 go 1.16
 
-require github.com/google/uuid v1.2.0
+require (
+	github.com/google/uuid v1.2.0
+	github.com/stretchr/testify v1.7.5
+)
--- uuid_validator.go
+++ uuid_validator.go
@@ -5,12 +5,9 @@ import (
 	"regexp"
 )
 
-type uuidValidator struct {
-	min int
-	max int
-}
+type uuidValidator struct{}
 
-func (v uuidValidator) validate(cid string, val interface{}) error{
+func (v uuidValidator) validate(cid string, val interface{}) error {
 	id := val.(string)
 	r := regexp.MustCompile("" +
 		"^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$",
