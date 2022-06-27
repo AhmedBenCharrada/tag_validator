9242434bd0202bbf7f12dc3a49e6ca2ac4de3790
ahmad <ahmed.b.charrada@gmail.com>
2022-06-27 16:30:47 +0100

    Modify tag validator

--- tag_validator.go
+++ tag_validator.go
@@ -1,8 +1,10 @@
 package tag_validator
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
+	"regexp"
 	"strings"
 )
 
@@ -10,13 +12,11 @@ import (
 // fields to be validated need to be tagged with this tag
 const tagName = "validate"
 
-// validator an interface that every tag validator should implement
-type validator interface {
-	// Validate checks whether the validated field is valid or not
-	validate(in interface{}) error
+type number interface {
+	int | int64
 }
 
-// ValidateStruct travers all the struct fields and validates attributes marked to be validated
+// ValidateStruct traverse all the struct fields and validates attributes marked to be validated
 func ValidateStruct(cid string, s interface{}) []error {
 	// ValueOf returns a Value representing the run-time data
 	v := reflect.ValueOf(s)
@@ -30,12 +30,7 @@ func ValidateStruct(cid string, s interface{}) []error {
 			continue
 		}
 		// Get a validator that corresponds to a tag
-		validator := getValidator(tag)
-		if validator == nil {
-			continue
-		}
-		// Perform validation
-		err := validator.validate(v.Field(i).Interface())
+		err := check(tag, v.Field(i).Interface())
 		// Append error to results
 		if err != nil {
 			errs = append(errs, fmt.Errorf("%s %s", v.Type().Field(i).Name, err.Error()))
@@ -45,20 +40,64 @@ func ValidateStruct(cid string, s interface{}) []error {
 }
 
 // gets the appropriate validator based on field type
-func getValidator(tag string) validator {
+func check(tag string, item interface{}) error {
 	args := strings.Split(tag, ",")
 	switch args[0] {
 	case "number":
-		validator := numberValidator{}
-		_, _ = fmt.Sscanf(strings.Join(args[1:], ","), "min=%d,max=%d", &validator.min, &validator.max)
-		return validator
-	case "string":
-		validator := stringValidator{}
-		_, _ = fmt.Sscanf(strings.Join(args[1:], ","), "min=%d,max=%d", &validator.min, &validator.max)
-		return validator
-	case "uuid":
-		return uuidValidator{}
+		var min int
+		var max int
+		_, _ = fmt.Sscanf(strings.Join(args[1:], ","), "min=%d,max=%d", &min, &max)
 
+		return validateNumber(min, max, item.(int))
+	case "string":
+		var min int
+		var max int
+		_, _ = fmt.Sscanf(strings.Join(args[1:], ","), "min=%d,max=%d", &min, &max)
+
+		return validateString(min, max, item.(string))
+	case "uuid":
+		return validateUUID(item.(string))
+	}
+
+	return nil
+}
+
+func validateNumber[T number](min, max, val T) error {
+	if val < min {
+		return fmt.Errorf("should be greater than %v", min)
+	}
+	// only max is defined (!= 0) and nun > v.max return an error
+	if max >= min && val > max {
+		return fmt.Errorf("should be less than %v", max)
+	}
+
+	return nil
+}
+
+func validateString(min int, max int, val string) error {
+	num := len(val)
+	if num == 0 {
+		return fmt.Errorf("should not be empty")
+	}
+
+	if num < min {
+		return fmt.Errorf("should be greater than %v", min)
+	}
+
+	// only max is defined (!= 0) and nun > v.max return an error
+	if max >= min && num > max {
+		return fmt.Errorf("should be less than %v", max)
+	}
+
+	return nil
+}
+
+func validateUUID(id string) error {
+	r := regexp.MustCompile("" +
+		"^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[8|9|aA|bB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$",
+	)
+	if !r.MatchString(id) {
+		return errors.New("invalid uuid")
 	}
 	return nil
 }
